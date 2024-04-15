97ae6e74ddef533b565410755dcf0d926656df9d
Angel Haller <angelhaller09@gmail.com>
2024-04-15 15:00:54 -0400

    Started researching and trying out the mapping step

--- run/runner.sh
+++ run/runner.sh
@@ -10,3 +10,20 @@ done
 #MULTIQC
 conda activate multiqc
 multiqc results/fastqc
+
+#---------------------------------------------------------
+#TRIMGALORE
+
+
+#--------------------------------------------------------
+#MAPPING
+
+#get annotated genome
+conda activate ncbi_datasets
+datasets download genome accession GCA_009761285.1 --filename PRSTRT_AglyBT1_v1
+
+#Lets try srnamapper instead of bowtie just for fun..
+conda activate srmamapper
+
+#test run with one
+perl sRNAmapper.pl -input data/raw/fastq/R1-1_S1_L001_R1_001.fastq.gz -genome data/ref/PRSTRT_AglyBT1_v1 -format sam -alignments best
