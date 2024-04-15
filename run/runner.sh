8f3c245ee427433a07baedf72e19720e65baf568
Angel Haller <angelhaller09@gmail.com>
2024-04-15 13:42:03 -0400

    Generated multiqc report for reads

--- run/runner.sh
+++ run/runner.sh
@@ -1,6 +1,12 @@
-module load fastqc/0.11.8
+#FASTQC ANALYSIS
 
+module load fastqc/0.11.8
 # Run FastQC for each FASTQ file
 for fastq_file in data/raw/fastq/*fastq.gz; do
     sbatch scripts/fastqc.sh "$fastq_file" results/fastqc
 done
+
+#----------------------------------------------------------
+#MULTIQC
+conda activate multiqc
+multiqc results/fastqc
